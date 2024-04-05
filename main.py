ff8b147df261a6e231dce17393e7ad86f238e3e5
Lawrence Lim <ll4715@nyu.edu>
2024-04-04 20:42:24 -0400

    add: created print function

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    print(
+        "Vishakh has a great life ... right? ... I think so. He's rly smart. Gets clowned on sometimes lol"
+    )
+
+
+if __name__ == "__main__":
+    main()
